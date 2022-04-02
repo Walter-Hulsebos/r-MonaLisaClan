bf82f90172a627f221238ed6521fe39302fdbbb7
Walter Hulsebos <77513543+Walter-Hulsebos@users.noreply.github.com>
2022-04-02 18:52:59 +0200

    Updated version Number

--- alliance_overlay.user.js
+++ alliance_overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MonaLisaClan
 // @namespace    http://tampermonkey.net/
-// @version      4.0
+// @version      5.0
 // @description  Keep the canvas beautiful!
 // @author       /u/djdogjuam2 & /u/irate_kalypso
 // @match        https://hot-potato.reddit.com/embed*
