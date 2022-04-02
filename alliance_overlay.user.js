5f8bd2cb9a448b023e09b47de2f19acd571fdcb6
Walter Hulsebos <77513543+Walter-Hulsebos@users.noreply.github.com>
2022-04-02 18:47:38 +0200

    Added Semi Transparent View

--- alliance_overlay.user.js
+++ alliance_overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MonaLisaClan
 // @namespace    http://tampermonkey.net/
-// @version      3.2
+// @version      4.0
 // @description  Keep the canvas beautiful!
 // @author       /u/djdogjuam2 & /u/irate_kalypso
 // @match        https://hot-potato.reddit.com/embed*
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://i.imgur.com/8tSGEg1.png";
+            i.src = "https://i.imgur.com/yZ7i3fC.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             
