5a3941d8f9c43b36a525c7c6eda5d5eebb34d2c8
Walter Hulsebos <77513543+Walter-Hulsebos@users.noreply.github.com>
2022-04-03 01:58:52 +0200

    DOUBLE SIZE

--- overlay_double_size.js
+++ overlay_double_size.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         MonaLisaClan
+// @name         MonaLisaClan DOUBLE SIZE
 // @namespace    http://tampermonkey.net/
-// @version      7.0
+// @version      1.0
 // @description  Keep the canvas beautiful!
 // @author       /u/djdogjuam2 & /u/irate_kalypso
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/Walter-Hulsebos/r-MonaLisaClan/blob/main/alliance_overlay.user.js
-// @downloadURL  https://github.com/Walter-Hulsebos/r-MonaLisaClan/raw/main/alliance_overlay.user.js
+// @updateURL    https://github.com/Walter-Hulsebos/r-MonaLisaClan/blob/main/overlay_double_size.js
+// @downloadURL  https://github.com/Walter-Hulsebos/r-MonaLisaClan/raw/main/overlay_double_size.js
 // @grant        none
 // ==/UserScript==
 if (window.top !== window.self) {
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/Walter-Hulsebos/r-MonaLisaClan/main/MonaLisaDesign.png";
+            i.src = "https://raw.githubusercontent.com/Walter-Hulsebos/r-MonaLisaClan/main/GIANT_LAYOUT.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             
